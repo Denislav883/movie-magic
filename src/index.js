ac11d013e10526d95e76fe89726c1cf902433f36
Denislav883 <yanev.denislav@gmail.com>
2025-09-25 10:02:25 +0300

    Add body parser

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ app.engine("hbs", handlebars.engine({
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
-app.use(express.static('src/public')); 
+app.use(express.static('src/public'));
+app.use(express.urlencoded());
 
 app.use(routes);
 
