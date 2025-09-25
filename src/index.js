03bd4b0a55feebe15eb03117347581cdc97805d3
Denislav883 <yanev.denislav@gmail.com>
2025-09-25 09:52:29 +0300

    Extract routes into separate module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import handlebars from "express-handlebars";
-import homeController from "./controllers/homeController.js";
-import movieController from "./controllers/movieController.js";
+import routes from "./router.js";
 
-const app = express();
+export const app = express();
 
 app.engine("hbs", handlebars.engine({
     extname: "hbs"
@@ -12,9 +11,8 @@ app.engine("hbs", handlebars.engine({
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
-app.use(express.static('src/public'));
+app.use(express.static('src/public')); 
 
-app.use(homeController);
-app.use(movieController);
+app.use(routes);
 
 app.listen(5000, console.log("Server is listening on http://localhost:5000..."));
