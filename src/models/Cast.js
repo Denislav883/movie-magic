68377f7e70f5297a033fbd835076cce20133d504
Denislav883 <yanev.denislav@gmail.com>
2025-10-09 21:22:34 +0300

    Validate cast

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -3,19 +3,26 @@ import { Schema, model } from "mongoose";
 const castSchema = new Schema({
     name: {
         type: String,
-        required: [true, "Cast name is required!"]
+        required: [true, "Cast name is required!"],
+        minLength: [5, "Name is too short!"],
+        match: [/^[a-zA-Z0-9 ]+$/, "Name has some invalid characters!"]
     },
     age: {
         type: Number,
-        required: [true, "Cast age is required!"]
+        required: [true, "Cast age is required!"],
+        max: [120, "Age cannot be more than 120!"],
+        min: [1, "Age cannot be less than 0!"]
     },
     born: {
         type: String,
-        required: [true, "Cast born is required!"]
+        required: [true, "Cast born is required!"],
+        minLength: [10, "Born should be at least 10 characters long!"],
+        match: [/^[a-zA-Z0-9 ]+$/, "Born has some invalid characters!"]
     },
     imageUrl: {
         type: String,
-        required: [true, "Cast imageUrl is required!"]
+        required: [true, "Cast imageUrl is required!"],
+        match: [/^https?:\/\//, "ImgaeUrl is invalid!"]
     }
 });
 
